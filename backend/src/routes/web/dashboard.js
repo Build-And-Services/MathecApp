cc2840e690233618c68e67fe26bce27b911ff708
Heri Setyawan <herisetyawan233@gmail.com>
2023-12-22 13:59:58 +0700

    feat/dashboard: get data at dashboard

--- backend/src/routes/web/dashboard.js
+++ backend/src/routes/web/dashboard.js
@@ -1,14 +1,31 @@
 const express = require('express');
 const router = express.Router();
 const verify = require('./../../middleware/verify');
+const { QuestionAnswer, Question, User } = require('@models');
 
 router.use(verify);
-router.get('/', (req, res) => {
-  res.render('index', {
-    title: 'Mathec | Dashboard',
-    page_name: 'dashboard',
-    admin: req.session.admin,
-  }); // Render file index.ejs dengan variabel 'nama'
+router.get('/', async (req, res) => {
+  try {
+    const [questionsCount, answersCount, usersCount] = await Promise.all([
+      Question.count(),
+      QuestionAnswer.count(),
+      User.count(),
+    ]);
+
+    res.render('index', {
+      title: 'Mathec | Dashboard',
+      page_name: 'dashboard',
+      admin: req.session.admin,
+      data: {
+        questions: questionsCount,
+        answers: answersCount,
+        users: usersCount,
+      },
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Terjadi kesalahan' });
+  }
 });
 
 module.exports = router;

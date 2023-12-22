efbffb5eb0f4e40c9d90048afd7a106a1f86700b
Heri Setyawan <herisetyawan233@gmail.com>
2023-12-22 14:16:11 +0700

    wip: get data at kuisioner

--- backend/models/questioner.js
+++ backend/models/questioner.js
@@ -8,7 +8,10 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      this.belongsTo(models.CategoryQuestioner, {
+        foreignKey: 'id_category_questioner',
+        as: 'category',
+      });
     }
   }
   Questioner.init(
--- backend/src/routes/web/kuisioner.js
+++ backend/src/routes/web/kuisioner.js
@@ -1,16 +1,32 @@
 const express = require('express');
 const router = express.Router();
 const verify = require('./../../middleware/verify');
+const { Questioner, CategoryQuestioner } = require('@models');
 
 router.use(verify);
-router.get('/', (req, res) => {
-  const nama = 'Pengguna';
-  res.render('kuisioner', {
-    nama,
-    title: 'Mathec | Kuisioner',
-    page_name: 'kuisioner',
-    admin: req.session.admin,
-  }); // Render file index.ejs dengan variabel 'nama'
+router.get('/', async (req, res) => {
+  try {
+    const kuisioners = await Questioner.findAll({
+      include: [
+        {
+          model: CategoryQuestioner,
+          as: 'category',
+          attributes: ['name'],
+        },
+      ],
+    });
+    const nama = 'Pengguna';
+    res.render('kuisioner', {
+      nama,
+      title: 'Mathec | Kuisioner',
+      page_name: 'kuisioner',
+      admin: req.session.admin,
+      kuisioners,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Terjadi kesalahan' });
+  }
 });
 
 module.exports = router;

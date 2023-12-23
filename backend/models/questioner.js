1cd9d62f96d56be964a193a2668b60371b2cd2fb
Heri Setyawan <herisetyawan233@gmail.com>
2023-12-23 17:30:36 +0700

    done/kuisioner: score, all questioner, user, print to pdf

--- backend/models/questioner.js
+++ backend/models/questioner.js
@@ -1,5 +1,5 @@
-'use strict';
-const { Model } = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Questioner extends Model {
     /**
@@ -9,8 +9,12 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       this.belongsTo(models.CategoryQuestioner, {
-        foreignKey: 'id_category_questioner',
-        as: 'category',
+        foreignKey: "id_category_questioner",
+        as: "category",
+      });
+      this.hasMany(models.LinkertScore, {
+        foreignKey: "id_questioner",
+        as: "linkertScore",
       });
     }
   }
@@ -21,10 +25,10 @@ module.exports = (sequelize, DataTypes) => {
     },
     {
       sequelize,
-      modelName: 'Questioner',
-      tableName: 'Questioners',
+      modelName: "Questioner",
+      tableName: "Questioners",
       underscored: true,
-    }
+    },
   );
   return Questioner;
 };
--- backend/src/routes/web/kuisioner.js
+++ backend/src/routes/web/kuisioner.js
@@ -1,31 +1,73 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const verify = require('./../../middleware/verify');
-const { Questioner, CategoryQuestioner } = require('@models');
+const verify = require("./../../middleware/verify");
+const {
+  User,
+  Questioner,
+  CategoryQuestioner,
+  LinkertScore,
+} = require("@models");
 
 router.use(verify);
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   try {
     const kuisioners = await Questioner.findAll({
       include: [
         {
           model: CategoryQuestioner,
-          as: 'category',
-          attributes: ['name'],
+          as: "category",
+          attributes: ["name"],
+        },
+        {
+          model: LinkertScore,
+          as: "linkertScore",
+          attributes: ["score"],
+          include: [
+            {
+              model: User,
+              as: "user",
+              attributes: ["name"],
+            },
+          ],
         },
       ],
     });
-    const nama = 'Pengguna';
-    res.render('kuisioner', {
+
+    const transformedData = kuisioners.map((d) => {
+      const total_score = d.linkertScore.map((d) => d.score);
+      const user = total_score.length;
+      const score = total_score.reduce((previous, current) => {
+        const sum = previous + current;
+        previous = sum;
+        return sum;
+      }, 0);
+
+      return {
+        id: d.id,
+        questioner: d.questioner,
+        category: d.category.name,
+        score,
+        total: user,
+        linkertScore: d.linkertScore.map((item) => {
+          return {
+            user: item.user.name,
+            score: item.score,
+          };
+        }),
+      };
+    });
+
+    const nama = "Pengguna";
+    res.render("kuisioner", {
       nama,
-      title: 'Mathec | Kuisioner',
-      page_name: 'kuisioner',
+      title: "Mathec | Kuisioner",
+      page_name: "kuisioner",
       admin: req.session.admin,
-      kuisioners,
+      kuisioners: transformedData,
     });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: 'Terjadi kesalahan' });
+    res.status(500).json({ message: "Terjadi kesalahan" });
   }
 });
 

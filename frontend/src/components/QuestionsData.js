1d67cecd40292a30f73331661421a881e6ef9d35
Heri Setyawan <herisetyawan233@gmail.com>
2023-12-29 00:41:44 +0700

    refactor: modal report as component

--- frontend/src/components/QuestionsData.js
+++ frontend/src/components/QuestionsData.js
@@ -4,40 +4,14 @@ import { Link } from 'react-router-dom';
 import { BsFillShareFill, BsExclamationCircleFill } from 'react-icons/bs';
 import { HiChatAlt2 } from 'react-icons/hi';
 import { useState } from 'react';
-import { Modal, Form, Spinner } from 'react-bootstrap';
+// import { Modal, Form, Spinner } from 'react-bootstrap';
+import ReportModal from './Report';
 
 const QuestionsData = ({ question, onLike, onDislike, onSave, name }) => {
   const [show, setShow] = useState(false);
-  const user = JSON.parse(localStorage.getItem('user'));
-  const [form, setForm] = useState({
-    jenis_laporan: '',
-    deskripsi: '',
-    bukti_laporan: '',
-    question_id: question.question_id,
-    pelapor_id: '',
-  });
-  const [loading, setLoading] = useState(false);
   const inputDate = new Date(question.posted_at);
   const options = { day: 'numeric', month: 'long', year: 'numeric' };
-  const [file, setFile] = useState();
-
-  const handleFileChange = (e) => {
-    const selectedFile = e.target.files[0];
-    setFile(selectedFile);
-  };
-
-  const handleClose = () => setShow(false);
-
-  const handleShow = () => {
-    if (user) {
-      setForm((prevstate) => {
-        return { ...prevstate, pelapor_id: user.data.user_id };
-      });
-      setShow(true);
-    } else {
-      alert('Please Login First for report question');
-    }
-  };
+  const formattedDate = inputDate.toLocaleDateString('id-ID', options);
 
   const handleShare = () => {
     if (navigator.share) {
@@ -59,37 +33,6 @@ const QuestionsData = ({ question, onLike, onDislike, onSave, name }) => {
     }
   };
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const formData = new FormData();
-    for (const key in form) {
-      formData.append(key, form[key]);
-    }
-
-    if (file) {
-      formData.append('file', file);
-    }
-    setLoading(true);
-    await fetch(process.env.REACT_APP_API_HOST + '/api/reports', {
-      method: 'POST',
-      headers: {
-        Authorization: `Bearer ${user.data.token}`,
-      },
-      body: formData,
-    });
-    setTimeout(() => {
-      setLoading(false);
-      handleClose();
-      setForm((prevstate) => ({
-        ...prevstate,
-        jenis_laporan: '',
-        deskripsi: '',
-      }));
-      setFile(null);
-    }, 500);
-  };
-
-  const formattedDate = inputDate.toLocaleDateString('id-ID', options);
   return (
     <>
       <div className='row mb-5'>
@@ -251,7 +194,7 @@ const QuestionsData = ({ question, onLike, onDislike, onSave, name }) => {
                             }}
                             data-toggle='modal'
                             data-target='#exampleModalCenter'
-                            onClick={handleShow}
+                            onClick={() => setShow(true)}
                           >
                             <BsExclamationCircleFill
                               style={{
@@ -279,70 +222,12 @@ const QuestionsData = ({ question, onLike, onDislike, onSave, name }) => {
           </div>
         </div>
       </div>
-      <Modal
+      <ReportModal
+        to='question'
+        id={question.question_id}
         show={show}
-        size='lg'
-        aria-labelledby='contained-modal-title-vcenter'
-        centered
-      >
-        <Modal.Header closeButton onClick={handleClose}>
-          <Modal.Title id='contained-modal-title-vcenter'>
-            Form Pelaporan
-          </Modal.Title>
-        </Modal.Header>
-        <form onSubmit={handleSubmit}>
-          <Modal.Body>
-            <Form.Group className='mb-3'>
-              <Form.Label>Jenis Pelanggaran</Form.Label>
-              <Form.Select
-                aria-label='Default select example'
-                onChange={(e) =>
-                  setForm((prevstate) => {
-                    return { ...prevstate, jenis_laporan: e.target.value };
-                  })
-                }
-              >
-                <option>Pilih Jenis Pelanggaran</option>
-                <option value='bersifat sara'>Bersifat Sara</option>
-                <option value='bersifat pornografi'>Bersifat Pornografi</option>
-                <option value='bersifat spam'>Bersifat Spam</option>
-                <option value='bersifat politik'>Bersifat Politik</option>
-                <option value='bersifat provokatif'>Bersifat Provokatif</option>
-              </Form.Select>
-            </Form.Group>
-            <Form.Group
-              className='mb-3'
-              controlId='exampleForm.ControlTextarea1'
-            >
-              <Form.Label>Deskripsi</Form.Label>
-              <Form.Control
-                as='textarea'
-                rows={3}
-                onChange={(e) =>
-                  setForm((prevstate) => {
-                    return { ...prevstate, deskripsi: e.target.value };
-                  })
-                }
-              />
-            </Form.Group>
-            <Form.Group className='mb-3'>
-              <Form.Label>Bukti</Form.Label>
-              <Form.Control type='file' onChange={handleFileChange} />
-            </Form.Group>
-          </Modal.Body>
-          <Modal.Footer>
-            <button type='submit' className='btn btn-primary'>
-              {loading ? (
-                <Spinner animation='border' role='status'>
-                  <span className='visually-hidden'>Loading...</span>
-                </Spinner>
-              ) : (
-                'Laporkan'
-              )}
-            </button>
-          </Modal.Footer>
-        </form>
-      </Modal>
+        setShow={setShow}
+      />
     </>
   );
 };
